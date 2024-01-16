2b8a1d9bde6ccbfb69cb2adff567f19c4da38edd
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-01-16 07:22:46 +0530

    16-01-24: List built-in functions

--- PythonTutorial/data_structures/my_list.py
+++ PythonTutorial/data_structures/my_list.py
@@ -6,9 +6,10 @@ List:
 a=[] #1. an empty list can be created
 print("a-->",a)
 print(type(a))
-b=[1,2,3,4] # 2. creation of list manually with elements
-print("b-->",b)
 '''
+b=[1,2,3,4, 4] # 2. creation of list manually with elements
+print("b-->",b)
+
 c=list(range(2, 21, 2)) #3. Creation list using built-in function
 print("c-->",c)
 '''
@@ -37,5 +38,25 @@ print(c[-2:-9:-1])
 print(c[::1])
 print(c[::-1])
 '''
+'''
 c[4]=15
 print("c after modifying-->",c) #6. List can be modifiable
+'''
+c.append(89)
+# print(c)
+# c.append(b)
+# print(c)
+# print(len(c))
+# print(c[10])
+# print(c[11])
+c.extend(b)
+print(c)
+# print(c[11])
+# print(c[13])
+# c.clear()
+# f=c.copy()
+# print(f)
+print(c.count(4))
+print(c.index(4,2))
+
+
