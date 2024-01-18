b70184514e0a9cba73102ada28d06a3e14b3e064
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-01-18 07:31:00 +0530

    18-01-24: Looping  statements

--- PythonTutorial/basics_tutorial/looping_statements.py
+++ PythonTutorial/basics_tutorial/looping_statements.py
@@ -4,24 +4,55 @@
         - initialization
         - condition statement
         - increment/ decrement
-    b. for loop
+    b. for loop- to iterate over the range or sequence
+    
+break, continue: loop controlling statements
 '''
 
-
+'''
 position=1
 while position<=5:
     
-    '''
+    
     age=int(input("Enter your age:"))
     if age>=13:
         print("Allow")   
     else:
         print("Don't allow")
-    '''
+    
     print("Position value:",position)
     position=position+1
     if position==3:
         continue
     print("new position value:", position)
+'''
+'''
+count=1
+while count<=5:
+    print("Hello world!")
     
+    if count==3:
+        break
+    count=count+1
+'''
+'''
+for count in range(5):
+    print("Hello world!")
     
+'''
+index=0
+a=[1,2, 4, 5, "abv", 3, 4, 5]
+for i in a:
+    if i=="abv":
+        continue
+    print(i)
+'''
+for i in a:
+    if i==5:
+        print(index, i)
+    index=index+1
+    # if i=="abv":
+    #     break
+    # print(type(i))
+    # print("Hello world!")
+'''
