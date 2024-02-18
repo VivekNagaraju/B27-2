5fc671e5f5f3313bcdec99fa00a32837338efb64
FalguniDeepthi <125749869+FalguniDeepthi@users.noreply.github.com>
2024-02-18 11:47:54 +0530

    Pushing last module

---
+++ PythonTutorial/basics_tutorial/test_module.py
@@ -0,0 +1,18 @@
+'''
+Created on 18-Feb-2024
+
+@author: admin
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
